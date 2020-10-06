1fcfbcb4163ac6443d5a173a0ba0cee3d5e5d2d3
anujjoshi <anuj@groktor.net>
2020-10-06 17:58:48 +0200

    Initial Commit

---
+++ SwiftUICustomTabBar/SwiftUICustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabBarApp.swift
+//  SwiftUICustomTabBar
+//
+//  Created by Anuj Joshi on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
