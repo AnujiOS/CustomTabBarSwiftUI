c597b668ebb564c84d699e81b5a5813e78bb6f02
anujjoshi <anuj@groktor.net>
2020-10-06 18:21:24 +0200

    Add CustomTabBarItem

---
+++ SwiftUICustomTabBar/CustomTabBarItem.swift
@@ -0,0 +1,65 @@
+//
+//  CustomTabBarItem.swift
+//  SwiftUICustomTabBar
+//
+//  Created by Anuj Joshi on 06/10/2020.
+//
+
+import SwiftUI
+
+struct CustomTabBarItem<Content: View>: View {
+    let iconName: String
+    let label: String
+    let selection: Binding<Int>
+    let tag: Int
+    let content: () -> Content
+
+    init(iconName: String,
+         label: String,
+         selection: Binding<Int>,
+         tag: Int,
+         @ViewBuilder _ content: @escaping () -> Content) {
+        self.iconName = iconName
+        self.label = label
+        self.selection = selection
+        self.tag = tag
+        self.content = content
+    }
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(systemName: iconName)
+                .frame(minWidth: 25, minHeight: 25)
+            Text(label)
+                .font(.caption)
+        }
+        .padding([.top, .bottom], 5)
+        .foregroundColor(fgColor())
+        .frame(maxWidth: .infinity)
+        .contentShape(Rectangle())
+        .onTapGesture { self.selection.wrappedValue = self.tag }
+        .preference(key: TabBarPreferenceKey.self,
+                    value: TabBarPreferenceData(
+                        tabBarItemData: [TabBarItemData(tag: tag,
+                                                        content: AnyView(self.content()) // 3
+                        )]
+                    )
+        )
+    }
+
+    private func fgColor() -> Color {
+        return selection.wrappedValue == tag ? Color(UIColor.systemBlue) : Color(UIColor.systemGray)
+    }
+}
+
+struct CustomTabBarItem_Previews: PreviewProvider {
+    static var selection: Int = 0
+    static var selectionBinding = Binding<Int>(get: { selection }, set: { selection = $0 })
+
+    static var previews: some View {
+        CustomTabBarItem(iconName: "clock.fill", label: "Recents", selection: selectionBinding, tag: 0) {
+            Text("Empty View")
+        }
+        .previewLayout(.fixed(width: 80, height: 80))
+    }
+}
